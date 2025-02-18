df294fb806118c04f0c2564c434045c94a20bcea
Ishiva Oliver <ishivaoliver@gmail.com>
2025-02-18 16:10:21 +0300

    changes in structure

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Initialize AOS (Animate on Scroll)
+  AOS.init({
+    offset: 200,
+    duration: 1000,
+    easing: "ease-in-out",
+    once: true,
+  });
+});
