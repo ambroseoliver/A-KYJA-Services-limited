af8c1669021039bbc843abbdbb7327910a727305
Ishiva Oliver <ishivaoliver@gmail.com>
2025-02-19 18:28:38 +0300

    Our serives section styling

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ document.addEventListener("DOMContentLoaded", function () {
     once: true,
   });
 });
+
+
+// Our services section 
+/* JavaScript Interactions */
+document.querySelectorAll('.col-md-4').forEach((card) => {
+  card.addEventListener('mousemove', (e) => {
+    let x = (e.offsetX / card.offsetWidth) * 30 - 15;
+    let y = (e.offsetY / card.offsetHeight) * 30 - 15;
+    card.style.transform = `rotateX(${y}deg) rotateY(${x}deg)`;
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = 'rotateX(0deg) rotateY(0deg)';
+  });
+});
